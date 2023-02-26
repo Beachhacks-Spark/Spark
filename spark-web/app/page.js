ed84e28bd0bd7d87899d44a01d06c0ad56bd341c
Eric Yun <yunseric@gmail.com>
2023-02-25 20:20:38 -0800

    added login/signup which need to be styled and basic homepage styling

--- spark-web/app/page.js
+++ spark-web/app/page.js
@@ -1,12 +1,21 @@
+'use client'
 import Image from 'next/image'
-import styles from './page.module.css'
-
+import Marquee from 'react-fast-marquee'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className = "grid align-items-center h-screen">
-        test
+    <main>
+      <div className = "flex items-center justify-center h-screen">
+        <Marquee
+          pauseOnHover = {true}
+          speed = {100}
+          className = "bg-transparent"
+          gradientColor={'transparent'}
+          gradient = {false}
+        >
+         <h1 className = "text-[250px] font-bold">SPARK &#9889; SPARK &#9889; SPARK &#9889;</h1>
+
+        </Marquee>
       </div>
     </main>
   )
